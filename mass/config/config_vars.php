f7baaa4532133e49c571433b010071dcb87fd83f
erjimmy796 <erjimmy796@hotmail.com>
2015-09-18 10:38:29 -0500

    Retiro de variales de conexion ...

--- mass/config/config_vars.php
+++ mass/config/config_vars.php
@@ -1,7 +1,3 @@
 <?php
-	$GLOBALS['rdbms'] = "mysql";
-	$GLOBALS['dbhost'] = "localhost";
-	$GLOBALS['dbname'] = "ospinas";
-	$GLOBALS['username'] = "ospinas";
-	$GLOBALS['userpass'] = "0sp1naS2015";
+	
 ?>
